63fa323b8e0cd9a0eb5481872bc9e82a80e57262
JuSung Lee <jusunglee1459@gmail.com>
2023-02-17 16:08:03 -0800

    await refactor for vote state

--- server/gameServer/game-server.js
+++ server/gameServer/game-server.js
@@ -224,211 +224,207 @@ async function checkEndGame() {
 async function checkState() {
     console.log("game state: " + game_state);
 
-        if (game_state == "end" || game_state == "end-mafia" || game_state == "end-town") {
-            process.exit(0);
-        } 
+    if (game_state == "end" || game_state == "end-mafia" || game_state == "end-town") {
+        process.exit(0);
+    } 
 
-        // todo make switch statement
-        if (game_state == "vote") {
-            // count up votes and put player on trial
-            let votes = new Map(Object.entries(game.data.votes));
-            let vote_counts = new Map();
-            // pool up counts
-            votes.forEach(function(value, key) {
-                if (vote_counts.has(value)) {
-                    vote_counts.set(value, (vote_counts.get(value) + 1));
-                } else {
-                    vote_counts.set(value, 1);
-                }
-            });
-            let lynch_player = null;
-            if (votes.size > 0) {
-                console.log(vote_counts)
-                let votesDesc = [...vote_counts.entries()].sort((a,b) => b[1] - a[1])
-                console.log(votesDesc);
-                if (votesDesc.length > 1) {
-                    if (votesDesc[0][1] > votesDesc[1][1]) {
-                        // lynch player
-                        console.log("first")
-                        lynch_player = votesDesc[0][0];
-                    } else {
-                        // tie vote, no player lynched
-                        console.log("second")
-                        next_game_state = "after_trial_talk";
-                    }
-                } else {
-                    // one vote, default lynch player
-                    console.log("third")
+    // todo make switch statement
+    if (game_state == "vote") {
+        // count up votes and put player on trial
+        let votes = new Map(Object.entries(game.data.votes));
+        let vote_counts = new Map();
+        // pool up counts
+        votes.forEach(function(value, key) {
+            if (vote_counts.has(value)) {
+                vote_counts.set(value, (vote_counts.get(value) + 1));
+            } else {
+                vote_counts.set(value, 1);
+            }
+        });
+        let lynch_player = null;
+        if (votes.size > 0) {
+            console.log(vote_counts)
+            let votesDesc = [...vote_counts.entries()].sort((a,b) => b[1] - a[1])
+            console.log(votesDesc);
+            if (votesDesc.length > 1) {
+                if (votesDesc[0][1] > votesDesc[1][1]) {
+                    // lynch player
+                    console.log("first")
                     lynch_player = votesDesc[0][0];
+                } else {
+                    // tie vote, no player lynched
+                    console.log("second")
+                    next_game_state = "after_trial_talk";
                 }
             } else {
-                next_game_state = "after_trial_talk";
+                // one vote, default lynch player
+                console.log("third")
+                lynch_player = votesDesc[0][0];
             }
-            // console.log(lynch_player)
-            if (lynch_player != null) {
-                // place player on trial
-                MafiaDB.updateOne({roomid:process.argv[2]},
-                    {$set:{"trial.trial_player":String(lynch_player),
-                        "votes":new Map()}
-                }).then((data) => {
-                    console.log(data)
-                }).catch(error => {
-                    console.log(error);
-                });
-            } else {
-                MafiaDB.updateOne({roomid:process.argv[2]},
-                    {$set:{"trial.trial_player":"",
-                        "votes":new Map()}
-                }).then((data) => {
-                    console.log(data)
-                }).catch(error => {
-                    console.log(error);
-                });
-            }
-             
+        } else {
+            next_game_state = "after_trial_talk";
         }
+        // console.log(lynch_player)
+        if (lynch_player != null) {
+            // place player on trial
+            MafiaDB.updateOne({roomid:process.argv[2]},
+                {$set:{"trial.trial_player":String(lynch_player),
+                    "votes":new Map()}
+            }).then((data) => {
+                console.log(data)
+            }).catch(error => {
+                console.log(error);
+            });
+        } else {
+            MafiaDB.updateOne({roomid:process.argv[2]},
+                {$set:{"trial.trial_player":"",
+                    "votes":new Map()}
+            }).then((data) => {
+                console.log(data)
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+            
+    }
 
-        if (game_state == "trial") {
-            // todo
-            // count up guilty vs innocents
-            // kill player or save player
-            console.log(game.data.trial)
+    if (game_state == "trial") {
+        // todo
+        // count up guilty vs innocents
+        // kill player or save player
+        console.log(game.data.trial)
 
-            // todo find player to lynch
-            let lynch_player = game.data.trial.trial_player;
+        // todo find player to lynch
+        let lynch_player = game.data.trial.trial_player;
 
 
-            if (lynch_player != null) {
-                let trial_votes = new Map(Object.entries(game.data.trial.votes));
-                let messages = [];
-                let vote_count_guilty = 0
-                let vote_count_inno = 0
-                game.data.players.forEach((player) => {
-                    console.log(player)
-                    if (player.living == true) {
-                        let message = { "nickname" : "Game",
-                                    "player_id" : "0"};
-                        if (trial_votes.has(player.player_id)) {
-                            if (trial_votes.get(player.player_id) == "guilty") {
-                                message["message"] = player.nickname + " voted guilty";
-                                vote_count_guilty++;
-                            } else if (trial_votes.get(player.player_id) == "inno") {
-                                vote_count_inno++;
-                                message["message"] = player.nickname + " voted innocent";                        }
-                        } else {
-                            message["message"] = player.nickname + " abstained"; 
-                        }
-                        messages.push(message)
+        if (lynch_player != null) {
+            let trial_votes = new Map(Object.entries(game.data.trial.votes));
+            let messages = [];
+            let vote_count_guilty = 0
+            let vote_count_inno = 0
+            game.data.players.forEach((player) => {
+                console.log(player)
+                if (player.living == true) {
+                    let message = { "nickname" : "Game",
+                                "player_id" : "0"};
+                    if (trial_votes.has(player.player_id)) {
+                        if (trial_votes.get(player.player_id) == "guilty") {
+                            message["message"] = player.nickname + " voted guilty";
+                            vote_count_guilty++;
+                        } else if (trial_votes.get(player.player_id) == "inno") {
+                            vote_count_inno++;
+                            message["message"] = player.nickname + " voted innocent";                        }
+                    } else {
+                        message["message"] = player.nickname + " abstained"; 
                     }
-                })
-                console.log(vote_count_guilty + " " + vote_count_inno)
-                console.log(messages);
-        
-                MafiaDB.updateOne({roomid:process.argv[2]}, {
-                    $push : { messages : { $each : messages } },
-                    $set : {trial : {votes : {}, trial_player: ""}}
-                }).then((data) => {
-                    console.log("trial messages");
-                    // process.send({action : "update_game"});
-                }).catch(error => {
-                    console.log(error);
-                });
-
-                if (vote_count_guilty > vote_count_inno) {
-                    // lynch player, set dead and delete from list
-                    MafiaDB.updateOne({roomid:process.argv[2], "players.player_id":String(lynch_player)},
-                        {$set:{"players.$.living":false}
-                    }).then((data)=> {
-                        return;
-                    }).catch(error => {
-                        console.log(error);
-                    });
-                    MafiaDB.updateOne({roomid:process.argv[2]},{
-                        $pull:{"game.good_players":lynch_player, "game.evil_players":lynch_player},
-                    }).catch(error => {
-                        console.log(error);
-                    });
-                    // process.send({action : "update_game"});
-                    
+                    messages.push(message)
                 }
-            }
-        }
-
-        if (game_state == "night") {
-            // todo
-            // see who used night roles and apply
-            // ranger - can investigate
-            // camper - can do nothing
-            // hunter - can kill a person once
-            // lumberjack - can protect
-            // sasquatchEVIL - can order kill
-            // littlefeetEVIL - can investigate
-            // bigfeetEVIL - does the dirty work
-
-            console.log(game.data.night)
-            if (game.data.night != undefined) {
-                const night_roles = new Map(Object.entries(game.data.night));
-                const roles = new Map(Object.entries(game.data.game.roles));
-
-                const role_investigation = {
-                    "ranger" : " upkeeps the park",
-                    "sasquatchEVIL" : " is very hairy",
-                    "camper" : " sing songs and roast marshmellows",
-                    "hunter" : " has a gun!", 
-                    "littlefeetEVIL": " is hairy with small feet", 
-                    "lumberjack" : " chop chop chops", 
-                    "bigfeetEVIL" : " has massive feet!"
-                };
-                await Promise.all(Array.from(night_roles).map(([role, value]) => {
-                    console.log("inside map")
-                    console.log(role);
-                    console.log(value);
-                    against_role = roles.get(value.against_id);
-                    const against_player_info = game.data.players.find(element => element.player_id == value.against_id);
-                    console.log(roles);
-                    console.log(against_player_info)
-                    if (role === 'ranger' || role === 'littlefeetEVIL') {
-                        // get against_id players role
-                        // send message through secret channel
-                        console.log("in ranger");
-                        const against_role = roles.get(value.against_id);
-                        let message = { "nickname" : "Game",
-                                        "player_id" : "0"};
-                        
-                        message["message"] = against_player_info.nickname + role_investigation[against_role];
+            })
+            console.log(vote_count_guilty + " " + vote_count_inno)
+            console.log(messages);
     
-                        return MafiaDB.updateOne({roomid:process.argv[2]}, {
-                            $push: {
-                                ["secret." + value.player_id] : message,
-                            }
-                        }).exec().catch(error => {
-                            console.log(error);
-                        });
-                    } else if (role === 'hunter' || role === 'sasquatchEVIL') {
-                        // kill player and remove from alive
-                        console.log("hunter: " );
-                        console.log(value)
-                        console.log(against_player_info);
-                        return MafiaDB.updateOne({roomid:process.argv[2], "players.player_id":value.against_id},
-                            {$set:{"players.$.living":false}
-                        }).then( (result) => {
-                            return MafiaDB.updateOne({roomid:process.argv[2]},{
-                                $pull:{"game.good_players":value.against_id, "game.evil_players":value.against_id},
-                            }).exec()
-                        }).catch(error => {
-                            console.log(error);
-                        });
-                    }
-                })).then((data) => {
-                    console.log("after await")
-                    console.log(data)
+            await MafiaDB.updateOne({roomid:process.argv[2]}, {
+                $push : { messages : { $each : messages } },
+                $set : {trial : {votes : {}, trial_player: ""}}
+            }).exec().catch(error => {
+                console.log(error);
+            });
+
+            if (vote_count_guilty > vote_count_inno) {
+                // lynch player, set dead and delete from list
+                const living_promise = MafiaDB.updateOne({roomid:process.argv[2], "players.player_id":String(lynch_player)},
+                    {$set:{"players.$.living":false}
+                }).exec().catch(error => {
+                    console.log(error);
+                });
+                const list_promise = MafiaDB.updateOne({roomid:process.argv[2]},{
+                    $pull:{"game.good_players":lynch_player, "game.evil_players":lynch_player},
+                }).exec().catch(error => {
+                    console.log(error);
                 });
 
+                await Promise.all([living_promise, list_promise])
                 
             }
-            
-
         }
+    }
+
+    if (game_state == "night") {
+        // todo
+        // see who used night roles and apply
+        // ranger - can investigate
+        // camper - can do nothing
+        // hunter - can kill a person once
+        // lumberjack - can protect
+        // sasquatchEVIL - can order kill
+        // littlefeetEVIL - can investigate
+        // bigfeetEVIL - does the dirty work
+
+        console.log(game.data.night)
+        if (game.data.night != undefined) {
+            const night_roles = new Map(Object.entries(game.data.night));
+            const roles = new Map(Object.entries(game.data.game.roles));
+
+            const role_investigation = {
+                "ranger" : " upkeeps the park",
+                "sasquatchEVIL" : " is very hairy",
+                "camper" : " sing songs and roast marshmellows",
+                "hunter" : " has a gun!", 
+                "littlefeetEVIL": " is hairy with small feet", 
+                "lumberjack" : " chop chop chops", 
+                "bigfeetEVIL" : " has massive feet!"
+            };
+            await Promise.all(Array.from(night_roles).map(([role, value]) => {
+                console.log("inside map")
+                console.log(role);
+                console.log(value);
+                against_role = roles.get(value.against_id);
+                const against_player_info = game.data.players.find(element => element.player_id == value.against_id);
+                console.log(roles);
+                console.log(against_player_info)
+                if (role === 'ranger' || role === 'littlefeetEVIL') {
+                    // get against_id players role
+                    // send message through secret channel
+                    console.log("in ranger");
+                    const against_role = roles.get(value.against_id);
+                    let message = { "nickname" : "Game",
+                                    "player_id" : "0"};
+                    
+                    message["message"] = against_player_info.nickname + role_investigation[against_role];
+
+                    return MafiaDB.updateOne({roomid:process.argv[2]}, {
+                        $push: {
+                            ["secret." + value.player_id] : message,
+                        }
+                    }).exec().catch(error => {
+                        console.log(error);
+                    });
+                } else if (role === 'hunter' || role === 'sasquatchEVIL') {
+                    // kill player and remove from alive
+                    console.log("hunter: " );
+                    console.log(value)
+                    console.log(against_player_info);
+                    return MafiaDB.updateOne({roomid:process.argv[2], "players.player_id":value.against_id},
+                        {$set:{"players.$.living":false}
+                    }).then( (result) => {
+                        return MafiaDB.updateOne({roomid:process.argv[2]},{
+                            $pull:{"game.good_players":value.against_id, "game.evil_players":value.against_id},
+                        }).exec()
+                    }).catch(error => {
+                        console.log(error);
+                    });
+                }
+            })).then((data) => {
+                console.log("after await")
+                console.log(data)
+            });
+
+            
+        }
+        
+
+    }
     return;
 }
 
