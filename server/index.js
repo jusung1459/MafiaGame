f58b070c5fe09bcf159e65a4bd69e368057f1f30
JuSung Lee <jusunglee1459@gmail.com>
2023-03-13 23:30:02 -0700

    bullmq setup

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-socketConnection = require('./helpers/socket-singleton')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+socketConnection = require('./helpers/socket-singleton');
 
-const app = express()
 
-const {db, redisClient } = require('./db')
-const mafiaRouter = require('./routes/mafia-router')
+// set up distributed queue
+
+
+
+// setup express server
+const app = express();
+
+const {db, redisClient } = require('./db');
+const mafiaRouter = require('./routes/mafia-router');
 
 require("dotenv").config();
 
-const apiPort = 3000
+const apiPort = 3000;
 
 var server = require('http').createServer(app);
 
@@ -22,13 +28,13 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use('/api', mafiaRouter)
+app.use('/api', mafiaRouter);
 
 socketConnection.connect(server);
 
-server.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+server.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
