2d5587ddc39ae1bba15107e1aa8b70708fa9a7b9
JuSung Lee <jusunglee1459@gmail.com>
2023-04-10 15:27:58 -0700

    error in night roles

--- server/gameServer/gameComponent/standardNightRoles/AbstractNightRole.js
+++ server/gameServer/gameComponent/standardNightRoles/AbstractNightRole.js
@@ -1,20 +1,30 @@
 // Abstract Night Role Class
 // Used to represent what a Role should do at night
 
+const { DataHandler } = require("../../helpers/DataHandler");
+
 class AbstractNightRole {
 
-    constructor() {
+    constructor(against_role, against_player_info, room_id) {
       if (new.target === AbstractNightRole) {
         throw new TypeError("Cannot construct Abstract instances directly");
       }
+
+      this.against_role = against_role;
+      this.against_player_info = against_player_info;
+      this.dataHandler = new DataHandler(room_id);
     }
 
 
-    message() {
+    messageAction() {
 
     }
 
     nightAction() {
+
+    }
+
+    async executeRole() {
 
     }
     
---
+++ server/gameServer/gameComponent/standardNightRoles/RangerNightRole.js
@@ -0,0 +1,34 @@
+const AbstractNightRole = require('./AbstractNightRole');
+const { DataHandler } = require("../../helpers/DataHandler");
+
+class RangerNightRole extends AbstractNightRole {
+
+    role_msg = " upkeeps the park";
+    message;
+
+    constructor(against_role, against_player_info, room_id, player) {
+        super(against_role, against_player_info, room_id);
+        this.player = player;
+    }
+
+
+    messageAction() {
+        this.message = { "nickname" : "Game",
+                        "player_id" : "0",
+                        "createdAt" : new Date()};
+                        
+        this.message["message"] = this.against_player_info.nickname + this.role_msg;
+        return;
+    }
+
+    nightAction() {
+        return;
+    }
+
+    async executeRole() {
+        return this.dataHandler.appendRoomData("$.secret." + this.player.player_id, this.message);
+    }
+    
+}
+
+module.exports = RangerNightRole;
