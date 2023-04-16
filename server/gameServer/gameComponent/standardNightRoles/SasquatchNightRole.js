b7dbd4c574d9d938e498b4723916adf4049200cb
JuSung Lee <jusunglee1459@gmail.com>
2023-04-16 00:29:50 -0700

    sasquatch night role

--- server/gameServer/gameComponent/standardNightRoles/SasquatchNightRole.js
+++ server/gameServer/gameComponent/standardNightRoles/SasquatchNightRole.js
@@ -6,29 +6,57 @@ class SasquatchNightRole extends AbstractNightRole {
     role_msg = " is hairy with small feet";
     message;
 
-    constructor(against_role, against_player_info, room_id, player) {
+    constructor(against_role, against_player_info, room_id, player, game, game_players) {
         super(against_role, against_player_info, room_id);
         this.player = player;
+        this.game = game; // this.game.game.
+        this.game_players = game_players; // this.game.players
     }
 
 
     messageAction() {
         this.message = { "nickname" : "Game",
-                        "player_id" : "0",
-                        "createdAt" : new Date()};
+                            "player_id" : "0"};
                         
         this.message["message"] = this.against_player_info.nickname + this.role_msg;
         return;
     }
 
     nightAction() {
-        return;
+        // remove from good_players or evil_players
+        console.log("REMOVE PLAYER");
+        // console.log(this.game);
+        let remove_player_index = this.game.good_players.findIndex((player) => {
+            console.log(player + " " + this.against_player_info.player_id)
+            return player == this.against_player_info.player_id
+        })
+        if (remove_player_index >= 0) {
+            this.game.good_players.splice(remove_player_index, 1);
+        } else {
+            remove_player_index = this.game.evil_players.findIndex((player) => {
+                return player == this.against_player_info.player_id
+            })
+            if (remove_player_index >= 0) {
+                this.game.evil_players.splice(remove_player_index, 1);
+            }
+        }
+        this.game.dead_players.push(this.against_player_info.player_id)
+
+        remove_player_index = this.game_players.findIndex((player) => {
+            console.log(player.player_id + " " + this.against_player_info.player_id)
+            return player.player_id == this.against_player_info.player_id
+        })
+
+        if (remove_player_index >= 0) {
+            this.game_players[remove_player_index].living = false
+        }
     }
 
     async executeRole() {
-        const dec_role = this.dataHandler.incrData('$.role_counter.littlefeetEVIL');
-        const add_secret_message = this.dataHandler.appendRoomData("$.secret." + this.player.player_id, this.message);
-        return Promise.all([dec_role, add_secret_message]);
+        const set_player_dead = this.dataHandler.addRoomData("$.players", this.game_players);
+        const set_game = this.dataHandler.addRoomData('$.game', this.game.game);
+        const add_mesg = this.dataHandler.appendRoomData('$.messages', this.message);
+        return Promise.all([set_player_dead, set_game, add_mesg])
     }
     
 }
